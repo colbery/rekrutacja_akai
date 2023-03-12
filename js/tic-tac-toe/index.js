6af9de8be321473a368a69a57bc003cbf50744f2
colbery <92644519+colbery@users.noreply.github.com>
2023-03-12 15:09:04 +0100

    0.3v

--- js/tic-tac-toe/index.js
+++ js/tic-tac-toe/index.js
@@ -1,5 +1,9 @@
 let turn = "x";
-let symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
+let symbols = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 const board = document.querySelector(".board");
 const tiles = Array.from(document.querySelectorAll(".tile"));
@@ -21,6 +25,8 @@ board.addEventListener("click", ({ target }) => {
 
 function displayTurn(turn) {
   // 1. zmień text elementu h1 z klasą "turn" zależnie od tego, czyja jest aktualnie tura
+  const whoseTurn = document.querySelector(".turn");
+  whoseTurn.innerText = turn.toUpperCase() + " turn ";
 }
 
 function checkWin() {
@@ -30,4 +36,5 @@ function checkWin() {
 // 3. dodaj listener pod przycisk z napisaem "reset" tak, aby po jego kliknięciu wywołać funkcję reset
 function reset() {
   // 4. zresetuj stan gry
+  console.log(symbols);
 }
